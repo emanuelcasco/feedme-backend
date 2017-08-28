7f84ec13d85d3e849f6478735f07639267d3852e
Emanuel <cascoemanuel@gmail.com>
2017-08-28 18:04:05 -0300

    Fixes

--- api/controllers/PriorityController.js
+++ api/controllers/PriorityController.js
@@ -4,8 +4,37 @@
  * @description :: Server-side logic for managing proyects
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+/* global Priority */
 
 module.exports = {
-
+  find (req, res) {
+    const id = req.params.id;
+    Priority.find(id).exec(function (err, results) {
+      if (results) {
+        if (results === []) {
+          res.status(201).json({ msg: 'Not found' });
+        } else {
+          const priority = results[0];
+          res.status(201).json(priority);
+        }
+      } else {
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+  },
+  findAll (req, res) {
+    Priority.find().exec(function (err, results) {
+      if (results) {
+        if (results === []) {
+          res.status(201).json({ msg: 'Not found' });
+        } else {
+          const priorities = results;
+          res.status(201).json(priorities);
+        }
+      } else {
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+  }
 };
 
